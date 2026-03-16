fe46f7480d24b8a4d307c02083aed052d286ac71
Conrad Calmez <conrad@calmez.net>
2026-03-16 09:27:03 +0100

    reworked tmux plugin handling

--- sync.sh
+++ sync.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 stow config --target ~/.config
 stow zsh --dotfiles
+
+if [[ ! -e ~/.config/tmux/plugins ]]; then
+  git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+fi
